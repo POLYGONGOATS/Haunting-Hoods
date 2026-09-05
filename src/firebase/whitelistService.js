bc9eea5f1e46e88399f39ffc213729752ede5979
Chitransh Raj <chitranshraj@192.168.1.2>
2026-09-05 12:18:07 +0530

    fix: bump slotsTotal to 4444

--- src/firebase/whitelistService.js
+++ src/firebase/whitelistService.js
@@ -121,7 +121,7 @@ export const claimWhitelistSpot = async ({
 			campaign = {
 				id: campaignId,
 				code: 'HAUNTED',
-				slotsTotal: 1000,
+				slotsTotal: 4444,
 				claimedCount: 0,
 				active: true,
 			};
@@ -133,9 +133,9 @@ export const claimWhitelistSpot = async ({
 				err.code = CLAIM_ERRORS.CAMPAIGN_INACTIVE;
 				throw err;
 			}
-			if (campaign.slotsTotal < 1000) {
-				campaign.slotsTotal = 1000;
-				transaction.update(campaignRef, { slotsTotal: 1000 });
+			if (campaign.slotsTotal < 4444) {
+				campaign.slotsTotal = 4444;
+				transaction.update(campaignRef, { slotsTotal: 4444 });
 			}
 		}
 
